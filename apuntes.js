d3c1db98e9bf32268905a5d132424c2014bab26b
unknown <alejandrojzeballos@gmail.com>
2021-12-20 22:30:46 -0400

    {add} paginas estaticas y dinamicas

--- apuntes.js
+++ apuntes.js
@@ -1,4 +1,6 @@
 /* apuntes curso Platzi
 
+Paginas estaticas: tienen información que no cambia, landing page, generado en el cliente.
+paginas dinamica: la información cambia,que consume endpoints, generado en el servidor.
 
 */
