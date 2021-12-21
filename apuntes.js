66820f264e3dc2d6be3c0220017524cf368d7fe9
unknown <alejandrojzeballos@gmail.com>
2021-12-20 22:37:02 -0400

    {add}html anatomia

--- apuntes.js
+++ apuntes.js
@@ -3,4 +3,10 @@
 Paginas estaticas: tienen información que no cambia, landing page, generado en el cliente.
 paginas dinamica: la información cambia,que consume endpoints, generado en el servidor.
 
+HTML--------------------------------
+hypertext markup languaje saltar de contenido con hiperlinks
+
+anatomia de una pagweb container-header-maincontent(sidebar, secciones)-footer
+
+
 */
